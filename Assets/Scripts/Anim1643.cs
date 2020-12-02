cf6e257d9b23e0a08f8b7f90e6d61ce206a2f24d
SitronX <citronovafuska@gmail.com>
2020-12-02 22:42:52 +0100

    1643 animation
    +Animation mostly finished

--- Assets/Scripts/Anim1643.cs
+++ Assets/Scripts/Anim1643.cs
@@ -21,13 +21,116 @@ public class Anim1643 : MonoBehaviour
 
     public void MainAnimStart()
     {
-        DefendingArmy[5].gameObject.transform.GetChild(0).GetComponent<Animator>().SetTrigger("Iddle");
-        DefendingArmy[6].gameObject.transform.GetChild(0).GetComponent<Animator>().SetTrigger("Iddle");
-        DefendingArmy[7].gameObject.transform.GetChild(0).GetComponent<Animator>().SetTrigger("Iddle");
+        gameObject.GetComponent<Animator>().SetTrigger("1643trigger");
+
+
+        //Defending ARMY-------------------------------------------------------------------------
+
+        StartCoroutine(SoldierDie(DefendingArmy[0], 25));
+
+        StartCoroutine(SoldierDie(DefendingArmy[2], 29));
+
+        StartCoroutine(SoldierDie(DefendingArmy[3], 37));
+
+        StartCoroutine(SoldierDie(DefendingArmy[4], 23));
+
+
+        StartCoroutine(SoldierIdle(DefendingArmy[5], 0));
+        StartCoroutine(SoldierAttack(DefendingArmy[5], 20));
+        StartCoroutine(SoldierDie(DefendingArmy[5], 27));
+
+
+
+        StartCoroutine(SoldierIdle(DefendingArmy[6], 0));
+        StartCoroutine(SoldierAttack(DefendingArmy[6], 28));
+        StartCoroutine(SoldierDie(DefendingArmy[6], 32));
+
+
+        StartCoroutine(SoldierIdle(DefendingArmy[7], 0));
+        StartCoroutine(SoldierAttack(DefendingArmy[7], 23));
+        StartCoroutine(SoldierIdle(DefendingArmy[7], 28));
+        StartCoroutine(SoldierAttack(DefendingArmy[7], 36));
+        StartCoroutine(SoldierDie(DefendingArmy[7], 38));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //ATTACKING ARMY----------------------------------------------------------------------------
+
+        StartCoroutine(CannonShot(cannonCrew, 9));
+
+        StartCoroutine(SoldierClimb(AttackingArmy[0], 20));
+        StartCoroutine(SoldierIdle(AttackingArmy[0], 26));
+
+
+        StartCoroutine(SoldierClimb(AttackingArmy[2], 21));
+        StartCoroutine(SoldierWalk(AttackingArmy[2], 25));
+        StartCoroutine(SoldierAttack(AttackingArmy[2], 35));
+
+
 
         StartCoroutine(SoldierDie(AttackingArmy[3], 4));
-        StartCoroutine(SoldierDie(AttackingArmy[5], 8));
-        StartCoroutine(CannonShot(cannonCrew, 9));
+
+        StartCoroutine(SoldierClimb(AttackingArmy[4], 19));
+        StartCoroutine(SoldierAttack(AttackingArmy[4], 25));
+        StartCoroutine(SoldierIdle(AttackingArmy[4], 34));
+
+
+
+        StartCoroutine(SoldierDie(AttackingArmy[5], 7));
+
+        StartCoroutine(SoldierClimb(AttackingArmy[6], 17));
+        StartCoroutine(SoldierDie(AttackingArmy[6], 25));
+
+
+
+        StartCoroutine(SoldierClimb(AttackingArmy[7], 17));
+        StartCoroutine(SoldierDie(AttackingArmy[7], 22));
+
+        StartCoroutine(SoldierClimb(AttackingArmy[8], 18));
+        StartCoroutine(SoldierAttack(AttackingArmy[8], 23));
+        StartCoroutine(SoldierWalk(AttackingArmy[8], 30));
+        StartCoroutine(SoldierAttack(AttackingArmy[8], 36));
+
+
+
+
+
+        StartCoroutine(SoldierClimb(AttackingArmy[9], 12));
+        StartCoroutine(SoldierAttack(AttackingArmy[9], 21));
+        StartCoroutine(SoldierIdle(AttackingArmy[9], 25));
+
+
+
+        StartCoroutine(SoldierClimb(AttackingArmy[10], 15));
+        StartCoroutine(SoldierIdle(AttackingArmy[10], 24));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 
@@ -37,6 +140,30 @@ public class Anim1643 : MonoBehaviour
 
         obj.transform.GetChild(0).GetComponent<Animator>().SetTrigger("Die");
     }
+    IEnumerator SoldierClimb(GameObject obj, int seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        obj.transform.GetChild(0).GetComponent<Animator>().SetTrigger("Climb");
+    }
+    IEnumerator SoldierAttack(GameObject obj, int seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        obj.transform.GetChild(0).GetComponent<Animator>().SetTrigger("Attack");
+    }
+    IEnumerator SoldierWalk(GameObject obj, int seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        obj.transform.GetChild(0).GetComponent<Animator>().SetTrigger("Walk");
+    }
+    IEnumerator SoldierIdle(GameObject obj, int seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        obj.transform.GetChild(0).GetComponent<Animator>().SetTrigger("Idle");
+    }
     IEnumerator CannonShot(GameObject obj, int seconds)
     {
         yield return new WaitForSeconds(seconds);
@@ -45,7 +172,7 @@ public class Anim1643 : MonoBehaviour
 
         yield return new WaitForSeconds(1);
 
-        obj.transform.GetChild(1).gameObject.transform.GetChild(0).GetComponent<Animator>().SetTrigger("Shot");
+        obj.transform.GetChild(1).gameObject.transform.GetChild(0).GetComponent<Animator>().SetTrigger("CannonShot");
 
 
 
