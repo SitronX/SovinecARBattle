baa80f6cae81dfc80014e8000f02ca9ffed16770
SitronX <citronovafuska@gmail.com>
2020-12-27 14:49:01 +0100

    Castle animation
    +Mining

---
+++ Assets/Scripts/Zari animace/rijen1.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rijen1 : Zari
+{
+    // Start is called before the first frame update
+    public Material transMat;
+    public Material normalMat;
+    public Animator animator;
+
+    private GameObject castle;
+
+    override
+    public void StartMethod()
+    {
+        animator.SetTrigger("Start");
+
+        castle = GameObject.Find("castle");
+        foreach(Transform i in castle.transform)
+        {
+            try
+            {
+                i.GetComponent<MeshRenderer>().material = transMat;
+            }
+            catch { }
+        }
+
+        StartCoroutine(FinishDay());
+
+
+
+        
+    }
+
+    IEnumerator FinishDay()
+    {
+        yield return new WaitForSeconds(20);
+
+        foreach (Transform i in castle.transform)
+        {
+            try
+            {
+                i.GetComponent<MeshRenderer>().material = normalMat;
+            }
+            catch { }
+        }
+
+
+    }
+
+
+}
